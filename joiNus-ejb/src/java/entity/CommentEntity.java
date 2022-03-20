d85944baf27f90d6d76ff9dd832eec32007b06d8
Jeremy Ong <e0540811@u.nus.edu>
2022-03-20 10:48:00 +0800

    reverted allocation old

---
+++ joiNus-ejb/src/java/entity/CommentEntity.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author Jeremy
+ */
+@Entity
+public class CommentEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+    @Column(nullable = false)
+    private String text;
+    @OneToOne
+    private NormalUserEntity commentOwner;
+
+    //Make Constructor
+    
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (commentId != null ? commentId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the commentId fields are not set
+        if (!(object instanceof CommentEntity)) {
+            return false;
+        }
+        CommentEntity other = (CommentEntity) object;
+        if ((this.commentId == null && other.commentId != null) || (this.commentId != null && !this.commentId.equals(other.commentId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.CommentEntity[ id=" + commentId + " ]";
+    }
+
+    /**
+     * @return the text
+     */
+    public String getText() {
+        return text;
+    }
+
+    /**
+     * @param text the text to set
+     */
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+}
