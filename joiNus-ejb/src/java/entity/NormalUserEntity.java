6a8d873839571c57783ae1b30be11b1e63c28923
shihan <wshihan@u.nus.edu>
2022-03-24 15:05:57 +0800

    removed unused imports from normal user entity

--- joiNus-ejb/src/java/entity/NormalUserEntity.java
+++ joiNus-ejb/src/java/entity/NormalUserEntity.java
@@ -5,11 +5,9 @@
  */
 package entity;
 
-import com.sun.istack.internal.Nullable;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale.Category;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
